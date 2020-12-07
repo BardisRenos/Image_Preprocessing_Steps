cdfb816fd27ef9929899d5c8f2267ee684c47b62
Renos Bardis <renosbardis@gmail.com>
2020-12-07 10:38:25 +0100

    The new feature

--- ImagePre.py
+++ ImagePre.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy
+import numpy as np
 
 
 def show_image(img):
@@ -8,9 +8,20 @@ def show_image(img):
     cv2.destroyAllWindows()
 
 
-img = cv2.imread('/home/eAdmin/Desktop/Images/100128_d6_front.png')
-print(img.shape)
+def add_noise(img):
+    from skimage.util import random_noise
+    # Adding Gaussian noise
+    noise_img = random_noise(img, mode='s&p', amount=0.3)
+    noise_img = np.array(255*noise_img, dtype='uint8')
+    show_image(noise_img)
 
 
-# if __name__ == "__main__":
-#     show_image(img)
+img = cv2.imread('/home/renos/Desktop/e-sante/e-sante/100128_d1_front.png')
+img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+# img_gray = cv2.resize(img_gray, (350, 350), interpolation=cv2.INTER_AREA)
+# print(img_gray.shape)
+
+# ret, thresh = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+if __name__ == "__main__":
+    add_noise(img_gray)
